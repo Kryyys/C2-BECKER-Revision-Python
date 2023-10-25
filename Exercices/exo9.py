7cd05a0a45a6437d03a4a0f90e169e9bc64b8f80
Marine <mariine.becker@gmail.com>
2023-10-25 12:44:03 +0200

    Exercice 9

---
+++ Exercices/exo9.py
@@ -0,0 +1,6 @@
+carre = [i ** 2 for i in range(1, 11)]
+print(carre)
+
+# J'aurais pu faire une liste
+# La range de 1 à 11 parce que si je mets juste 11, ça compte le 0
+#  ** est un opérateur de déballage qui calcule le carré d'un nombre
