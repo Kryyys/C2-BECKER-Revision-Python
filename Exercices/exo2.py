740ee0e2709af8b2a296d0331843ae79cba36fd7
Marine <mariine.becker@gmail.com>
2023-10-25 10:12:59 +0200

    réalisation de l'exo 2

---
+++ Exercices/exo2.py
@@ -0,0 +1,14 @@
+random = input("Veillez taper une phrase random")
+
+# Mettre la phrase en majuscule
+# maj = random.upper()
+# print(maj)
+
+# Mettre la phrase en minuscule
+# min = random.lower()
+# print(min)
+
+# Compter le nombre de mots
+mots = random.split()
+nombre = len(mots)
+print("Nombre de mots dans la phrase : " + str(nombre))
